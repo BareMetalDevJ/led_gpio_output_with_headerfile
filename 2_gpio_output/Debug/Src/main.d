73b8981cd56203aaa3754343c1a072668a6702e5
BareMetalDevJ <110313330+BareMetalDevJ@users.noreply.github.com>
2022-08-06 17:06:38 -0700

    Project files
    Here you can find all Project files

---
+++ 2_gpio_output/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ /Users/jamesjakubczyk/Downloads/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /Users/jamesjakubczyk/Downloads/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ /Users/jamesjakubczyk/Downloads/chip_headers/CMSIS/Include/core_cm4.h \
+ /Users/jamesjakubczyk/Downloads/chip_headers/CMSIS/Include/cmsis_version.h \
+ /Users/jamesjakubczyk/Downloads/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ /Users/jamesjakubczyk/Downloads/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ /Users/jamesjakubczyk/Downloads/chip_headers/CMSIS/Include/mpu_armv7.h \
+ /Users/jamesjakubczyk/Downloads/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+/Users/jamesjakubczyk/Downloads/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+/Users/jamesjakubczyk/Downloads/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+/Users/jamesjakubczyk/Downloads/chip_headers/CMSIS/Include/core_cm4.h:
+/Users/jamesjakubczyk/Downloads/chip_headers/CMSIS/Include/cmsis_version.h:
+/Users/jamesjakubczyk/Downloads/chip_headers/CMSIS/Include/cmsis_compiler.h:
+/Users/jamesjakubczyk/Downloads/chip_headers/CMSIS/Include/cmsis_gcc.h:
+/Users/jamesjakubczyk/Downloads/chip_headers/CMSIS/Include/mpu_armv7.h:
+/Users/jamesjakubczyk/Downloads/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
